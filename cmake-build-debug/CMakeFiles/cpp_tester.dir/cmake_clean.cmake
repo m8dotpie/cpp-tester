b73c16484a9133d13566f8d0fc7753ac388c9991
m8dotpie <alentev.olm@ya.ru>
2021-01-15 13:00:12 +0300

    Initial

---
+++ cmake-build-debug/CMakeFiles/cpp_tester.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_tester.dir/main.cpp.o"
+  "cpp_tester"
+  "cpp_tester.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_tester.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
